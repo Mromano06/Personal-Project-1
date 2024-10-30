d2264276f4aaa021c33f02f31abd2e24576f8029
Matthew Romano <matbat2205@gmail.com>
2024-10-30 18:31:57 -0400

    First commit, no real changes

---
+++ First Project/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Matthew Romano - October 30th, 2024
+
+// Main class for my first project
+
+// Trying to make a game in base C (no extrenal libraries)
+
+int main(void) {
+
+	printf("My name is Matthew Romano and I'm a Computer Science student at conestoga!");
+
+
+	return 0;
+}
