785a3e371d36d0c10f7a4c2302ab830a5306840c
Matthew Romano <matbat2205@gmail.com>
2024-11-01 21:30:01 -0400

    Added tests for the playerHealed function

--- First Project/playerHealed.c
+++ First Project/playerHealed.c
@@ -9,12 +9,12 @@ int playerHealed(int playerHealth, int healNum) {
 
 	int newPH = 0, pH = playerHealth, h = healNum;
 	
-	if (pH > 0 && h >+ 0) {		// checks if pH is over 0 and h isnt a negative value
+	if (pH > 0 && h >= 0) {		// checks if pH is over 0 and h isnt a negative value
 		newPH = pH + h;
 		return newPH;			// returns the new health
 	}
-		else
-			return -1;			// returns -1 if the player has 0 or less health
 
-	}
+	else
+		return -1;				// returns -1 if the player has 0 or less health
+
 }
--- PlayerFunctionTests/PlayerFunctionTests.cpp
+++ PlayerFunctionTests/PlayerFunctionTests.cpp
@@ -1,7 +1,12 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+// Matthew Romano - November 1st, 2024
+
+// unit tests for the player functions
+
 extern "C" int playerDamaged(int, int);
+extern "C" int playerHealed(int, int);
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -10,7 +15,8 @@ namespace PlayerFunctionTests
 	TEST_CLASS(PlayerFunctionTests)
 	{
 	public:
-		
+
+		// playerDamaged tests 
 		TEST_METHOD(PlayerHp10_DamageDone5_Retrun5)		// tests a "regular attack"
 		{
 			int health = 10, damage = 5, actual = 0, expected = 5;
@@ -22,6 +28,21 @@ namespace PlayerFunctionTests
 		{
 			int health = 1, damage = 10, actual = 0, expected = -1;
 			actual = playerDamaged(health, damage);
+			Assert::AreEqual(expected, actual);
+		}
+
+		// playerHealed tests 
+		TEST_METHOD(PlayerHp5_HealthGained5_Return10)	// tests a "regular heal"
+		{
+			int health = 5, heal = 5, actual = 0, expected = 10;
+			actual = playerHealed(health, heal);
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(PlayerHp1_HealthGainedN1_ReturnN1)	// tests if -1 is returned when heal amount is < 0
+		{
+			int health = 1, heal = -1, actual = 0, expected = -1;
+			actual = playerHealed(health, heal);
 			Assert::AreEqual(expected, actual);
 		}
 	};
